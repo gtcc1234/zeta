59f431dc3222c05f0cbe6f49380777819f58c5b4
GTC <ger@GTCs-MacBook-Pro.local>
2017-08-16 23:22:24 +0800

    add

--- src/Store/index.js
+++ src/Store/index.js
@@ -6,19 +6,41 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    loadedArticles: [],
+    loadedContents: [
+      {
+        imageUrl: 'https://bitcoin.org/img/icons/opengraph.png', id: '1', title: 'This is the headline for article 1', date: '2017-07-17', link: 'https://bitcoin.org', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqL_dAHmx6dx9W1ITcKJFryZEuLJYavVHJuDDiXu6lq85db8fMtQ', id: 'aaaa41231123aa2', title: 'This is the headline for article 2', date: '2017-07-19', link: 'https://www.ethereum.org', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: 'http://technode.com/wp-content/uploads/2015/12/technode-logo-ob-200.png', id: '3', title: 'This is the headline for article 3', date: '2017-07-04', link: 'http://technode.com/2017/07/20/ant-financial-invests-shanghai-based-fintech-startup-vfinance/', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: 'http://www.businessinsider.com/', id: '4', title: 'This is the headline for article 4', date: '2017-07-10', link: 'http://www.businessinsider.com/global-fintech-funding-rebounds-2017-7', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: '', id: '5', title: 'This is the headline for article 5', date: '2017-07-31', link: 'http://technode.com/2017/07/20/ant-financial-invests-shanghai-based-fintech-startup-vfinance/', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: '', id: '6', title: 'This is the headline for article 6', date: '2017-07-30', link: 'http://technode.com/2017/07/20/ant-financial-invests-shanghai-based-fintech-startup-vfinance/', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      },
+      {
+        imageUrl: '', id: '7', title: 'This is the headline for article 7', date: '2017-07-29', link: 'http://technode.com/2017/07/20/ant-financial-invests-shanghai-based-fintech-startup-vfinance/', author: 'John Doe', publication: 'Publication', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+      }
+    ],
     user: null
   },
   mutations: {
-    createArticle (state, payload) {
-      state.loadedArticles.push(payload)
+    createContent (state, payload) {
+      state.loadedContents.push(payload)
     },
     setUser (state, payload) {
       state.user = payload
     }
   },
   actions: {
-    createArticle ({commit}, payload) {
+    createContent ({commit}, payload) {
       const content = {
         title: payload.title,
         link: payload.link,
@@ -29,11 +51,12 @@ export const store = new Vuex.Store({
         date: payload.date
       }
       // Reach out to firebase to store
+      // Reach out to firebase to store
       let articlesRef = firebase.database().ref('Articles')
       let articleKey = articlesRef.push(content).key
       let newID = { id: articleKey }
       articlesRef.child(articleKey).update(newID)
-      commit('createArticle', content)
+      commit('createContent', content)
     },
     createUser ({commit}, payload) {
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password).then(user => {
@@ -48,12 +71,12 @@ export const store = new Vuex.Store({
     }
   },
   getters: {
-    loadedArticles (state) {
-      return state.loadedArticle.sort((contentA, contentB) => {
+    loadedContents (state) {
+      return state.loadedContents.sort((contentA, contentB) => {
         return contentA.date > contentB.date
       })
     },
-    featuredArticles (state, getters) {
+    featuredContents (state, getters) {
       return getters.loadedContents.slice(0, 6)
     },
     editContents (state, getters) {
@@ -62,10 +85,10 @@ export const store = new Vuex.Store({
     user (state) {
       return state.user
     },
-    loadedArticle (state) {
-      return (ArticleId) => {
-        return state.loadedArticles.find((content) => {
-          return content.id === ArticleId
+    loadedContent (state) {
+      return (contentId) => {
+        return state.loadedContents.find((content) => {
+          return content.id === contentId
         })
       }
     }
